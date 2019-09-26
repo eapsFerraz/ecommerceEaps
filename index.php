b4445f12068aa31d78efb1f4e9f1189df8edc8a8
Alessandro Ferraz <eaps@eaps.com.br>
2019-09-26 11:45:03 -0300

    TESTE

--- index.php
+++ index.php
@@ -10,4 +10,9 @@ $app->get('/', function (){
    $page = new Page();
    $page->setTpl("index");
 });
+
+$app->get('/admin', function (){
+    $page = new PageAdmin();
+    $page->setTpl("index");
+});
 $app->run();
