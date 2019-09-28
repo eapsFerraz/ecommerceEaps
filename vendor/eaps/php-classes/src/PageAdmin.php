e56b4d4e41525a9253fdff4824731abb4538c511
Alessandro Ferraz <eaps@eaps.com.br>
2019-09-28 18:26:56 -0300

    TESTE

--- vendor/eaps/php-classes/src/PageAdmin.php
+++ vendor/eaps/php-classes/src/PageAdmin.php
@@ -1,9 +1,10 @@
 <?php
 
+use Rain\Tpl;
 
 class PageAdmin extends Page
 {
-    public function __construct($opts = array(), $tpl_dir = "/views/admin")
+    public function __construct($opts = array(), $tpl_dir = "/views/admin/")
     {
         parent::__construct($opts, $tpl_dir);
     }
